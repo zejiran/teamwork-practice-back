0db140d998049ef5c785c86407cfd350d1c68b69
José Bocanegra <josejbocanegra@gmail.com>
2021-08-11 10:25:02 -0500

    Update ApplicationConfig.java

--- src/main/java/co/edu/uniandes/dse/treeking/config/ApplicationConfig.java
+++ src/main/java/co/edu/uniandes/dse/treeking/config/ApplicationConfig.java
@@ -3,11 +3,25 @@ package co.edu.uniandes.dse.treeking.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ApplicationConfig {
-	 @Bean
-	   public ModelMapper modelMapper() {
-	      return new ModelMapper();
-	   }
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE")
+						.maxAge(3600);
+			}
+
+		};
+	}
 }
