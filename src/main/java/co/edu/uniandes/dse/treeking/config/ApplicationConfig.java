3a1886ed77a388779af79b3489266a8958de6ff2
José Bocanegra <josejbocanegra@gmail.com>
2021-08-19 11:08:36 -0500

    Update ApplicationConfig.java

--- src/main/java/co/edu/uniandes/dse/treeking/config/ApplicationConfig.java
+++ src/main/java/co/edu/uniandes/dse/treeking/config/ApplicationConfig.java
@@ -18,7 +18,7 @@ public class ApplicationConfig {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE")
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE")
 						.maxAge(3600);
 			}
 
