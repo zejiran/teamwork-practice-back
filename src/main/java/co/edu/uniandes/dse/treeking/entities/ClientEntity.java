2bbea186f2c4c28e44f364283e8db6433808cdad
TrashCam9 <mc.gomezh1@uniandes.edu.co>
2021-08-26 21:42:44 -0500

    ClientEntity implementada junto con sus pruebas, logica y persistencia

---
+++ src/main/java/co/edu/uniandes/dse/treeking/entities/ClientEntity.java
@@ -0,0 +1,24 @@
+package co.edu.uniandes.dse.treeking.entities;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+import uk.co.jemos.podam.common.PodamExclude;
+
+@Entity
+@Getter
+@Setter
+public class ClientEntity extends BaseEntity{
+	
+	@PodamExclude
+	@OneToOne(cascade = CascadeType.ALL)
+	private CalendarEntity calendar;
+	
+	@PodamExclude
+	@ManyToOne
+	protected TransactionEntity transactions;
+}
---
+++ src/main/java/co/edu/uniandes/dse/treeking/services/ClientService.java
@@ -0,0 +1,27 @@
+package co.edu.uniandes.dse.treeking.services;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.edu.uniandes.dse.treeking.repositories.ClientRepository;
+import co.edu.uniandes.dse.treeking.entities.ClientEntity;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	ClientRepository clientRepository;
+	
+	/**
+	 * Obtiene la lista de los registros Client
+	 * @return Colección de objetos ClientEntity
+	 */
+	@Transactional
+	public List<ClientEntity> getClients(){
+		return clientRepository.findAll();
+	}
+}
