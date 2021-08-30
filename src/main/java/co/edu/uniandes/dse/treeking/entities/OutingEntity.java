6042641a33f0efd53724ac65e91aa85ecc6bfa15
TrashCam9 <mc.gomezh1@uniandes.edu.co>
2021-08-30 01:26:21 -0500

    OutingEntity implementada junto con lógica, persistencia y pruebas.

---
+++ src/main/java/co/edu/uniandes/dse/treeking/entities/OutingEntity.java
@@ -0,0 +1,68 @@
+package co.edu.uniandes.dse.treeking.entities;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import co.edu.uniandes.dse.treeking.podam.DateStrategy;
+import lombok.Getter;
+import lombok.Setter;
+import uk.co.jemos.podam.common.PodamExclude;
+import uk.co.jemos.podam.common.PodamStrategyValue;
+
+@Getter
+@Setter
+@Entity
+public class OutingEntity extends BaseEntity{
+	private String name;
+	
+	@Temporal(TemporalType.DATE)
+	@PodamStrategyValue(DateStrategy.class)
+	private Date date;
+	
+	private String place;
+	
+	private Integer participationCost;
+	
+	@PodamExclude
+	@OneToMany(mappedBy = "outing", 
+			   cascade = CascadeType.PERSIST, 
+			   fetch = FetchType.LAZY, 
+			   orphanRemoval = true)
+	private List<RouteEntity> route;
+	
+	@PodamExclude
+	@OneToOne
+	private ComplementaryInformationEntity complementaryInformation;
+	
+	@PodamExclude
+	@OneToOne
+	private ItineraryEntity itinerary;
+	
+	@PodamExclude
+	@OneToOne
+	private ReviewEntity review;
+	
+	@PodamExclude
+	@ManyToOne
+	private ActivityEntity activities;
+	
+	@PodamExclude
+	@ManyToOne
+	private GuideEntity guides;
+	
+	@PodamExclude
+	@OneToMany(mappedBy = "outing",
+			   fetch = FetchType.LAZY)
+	private List<QuotationEntity> quotation;
+
+	
+}
---
+++ src/main/java/co/edu/uniandes/dse/treeking/repositories/OutingRepository.java
@@ -0,0 +1,11 @@
+package co.edu.uniandes.dse.treeking.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import co.edu.uniandes.dse.treeking.entities.OutingEntity;
+
+@Repository
+public interface OutingRepository extends JpaRepository<OutingEntity, Long>{
+
+}
