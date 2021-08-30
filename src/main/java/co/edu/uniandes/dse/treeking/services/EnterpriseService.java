f4be94ccbeddc94530875f272c9f9a0f5e589ece
Phelyxx <habbowalkvps@gmail.com>
2021-08-30 15:44:33 -0500

    EnterpriseEntity con su persistencia, logica y tests
    Esta entidad hereda de ClientEntity y tiene una relación con PersonEntity, faltan estas clases para completarla.

---
+++ src/main/java/co/edu/uniandes/dse/treeking/services/EnterpriseService.java
@@ -0,0 +1,21 @@
+package co.edu.uniandes.dse.treeking.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import co.edu.uniandes.dse.treeking.entities.EnterpriseEntity;
+import co.edu.uniandes.dse.treeking.repositories.EnterpriseRepository;
+
+@Service
+public class EnterpriseService {
+	@Autowired
+	private EnterpriseRepository enterpriseRepository;
+	
+	@Transactional
+	public List<EnterpriseEntity> getEnterprises() {
+		return enterpriseRepository.findAll();
+	}
+}
