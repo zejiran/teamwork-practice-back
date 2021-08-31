3aac4f6dfcc57414754680d030e21d189663569e
Net <j.diazi@uniandes.edu.co>
2021-08-31 16:11:00 -0500

    admin,logica y consistencia

---
+++ src/main/java/co/edu/uniandes/dse/treeking/entities/AdminEntity.java
@@ -0,0 +1,18 @@
+package co.edu.uniandes.dse.treeking.entities;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+public class AdminEntity extends BaseEntity{ // TODO: extender de UserEntity
+
+    public void programarSalida(){ // TODO: poner los parametros route, fecha,
+
+    }
+
+}
