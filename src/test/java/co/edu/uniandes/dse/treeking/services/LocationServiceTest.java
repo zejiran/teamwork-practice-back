e158a315032bdf90fde06a87931d6e768254d92b
estudiante <jm.camachop1@uniandes.edu.co>
2021-08-31 03:59:05 -0500

    Configuré Location con pruebas y lógica. Las pruebas aún no corren.

---
+++ src/test/java/co/edu/uniandes/dse/treeking/services/LocationServiceTest.java
@@ -0,0 +1,85 @@
+package co.edu.uniandes.dse.treeking.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import co.edu.uniandes.dse.treeking.entities.LocationEntity;
+
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+
+
+/**
+ * Pruebas de lógica de Location
+ * @author BalaclavaAM
+ *
+ */
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@Transactional
+@Import(LocationService.class)
+class LocationServiceTest {
+	
+	@Autowired
+	private LocationService locationService;
+	
+	@Autowired
+	private TestEntityManager entityManager;
+	
+	private PodamFactory factory = new PodamFactoryImpl();
+	
+	private List<LocationEntity> listadeLocaciones = new ArrayList<>();
+	
+	/**
+	 * Configuración inicial del test.
+	 */
+	@BeforeEach
+	void instalacion() {
+		limpiarData();
+		insertarData();
+	}
+	
+	private void limpiarData() {
+		entityManager.getEntityManager().createQuery("DELETE FROM LocationEntity WHERE 1");
+	}
+	
+	private void insertarData() {
+		for (int i = 0; i < 3; i++) {
+			LocationEntity locationEntity = factory.manufacturePojo(LocationEntity.class);
+			entityManager.persist(locationEntity);
+			listadeLocaciones.add(locationEntity);
+		}
+	}
+	/**
+	 * Test para consultar la lista de locaciones
+	 */
+	@Test
+	void testConsultaLocaciones() {
+		List<LocationEntity> list = locationService.getLocations();
+		assertEquals(listadeLocaciones.size(), list.size());
+		for (LocationEntity entidad : list) {
+			boolean encontrao = false;
+			for (LocationEntity entidadGuardada : listadeLocaciones ) {
+				if (entidad.getId().equals(entidadGuardada.getId())) {
+					encontrao = true;
+				}
+			}
+			assertTrue(encontrao);
+		}
+	}
+
+}
