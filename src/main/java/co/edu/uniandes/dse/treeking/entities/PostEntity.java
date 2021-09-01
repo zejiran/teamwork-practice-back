8e8a4eeed0f1fa91bc3da46bc21f66e012a5a3b3
Phelyxx <habbowalkvps@gmail.com>
2021-09-01 18:43:00 -0500

    Remove attribute comments

--- src/main/java/co/edu/uniandes/dse/treeking/entities/PostEntity.java
+++ src/main/java/co/edu/uniandes/dse/treeking/entities/PostEntity.java
@@ -1,8 +1,11 @@
 package co.edu.uniandes.dse.treeking.entities;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -15,31 +18,22 @@ public class PostEntity extends BaseEntity {
 	
 	private Integer ratings;
 	
-	
-	// Implementar clases
-	
-	/*
 	@PodamExclude
-	@ManyToOne
-	private MultimediaEntity multimedia;
-	*/
+	@OneToMany(mappedBy = "post", fetch = FetchType.LAZY)
+	private List<MultimediaEntity> multimedia;
 	
-	/*
+
 	@PodamExclude
 	@ManyToOne
 	private BlogEntity blog;
-	*/
-	
-	/*
+
+
 	@PodamExclude
 	@ManyToOne
 	private CommentEntity comment;
-	*/
 	
-	/*
+
 	@PodamExclude
 	@ManyToOne
 	private UserEntity user;
-	*/
-
 }
