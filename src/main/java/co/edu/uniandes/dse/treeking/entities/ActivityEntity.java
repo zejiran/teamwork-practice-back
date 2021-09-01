dcc9d84973dae745e02fbd511383988680554181
Santiago Pardo <s.pardob@uniandes.edu.co>
2021-08-31 21:32:14 -0500

    ActivityEntity implementada con pruebas

---
+++ src/main/java/co/edu/uniandes/dse/treeking/entities/ActivityEntity.java
@@ -0,0 +1,23 @@
+package co.edu.uniandes.dse.treeking.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter 
+public class ActivityEntity extends BaseEntity {
+	
+	private String name;
+	
+	private String description;
+	
+	private LocalDateTime time;
+	
+	private Integer minutes;
+	
+}
