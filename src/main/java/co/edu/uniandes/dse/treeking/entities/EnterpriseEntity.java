2998897a539d271321392704004a45c9191e6d8e
Phelyxx <habbowalkvps@gmail.com>
2021-09-01 12:24:41 -0500

    Atributo PersonEntity añadido

--- src/main/java/co/edu/uniandes/dse/treeking/entities/EnterpriseEntity.java
+++ src/main/java/co/edu/uniandes/dse/treeking/entities/EnterpriseEntity.java
@@ -1,14 +1,24 @@
 package co.edu.uniandes.dse.treeking.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
 import lombok.Getter;
 import lombok.Setter;
+import uk.co.jemos.podam.common.PodamExclude;
+
+/**
+ * Clase que representa una empresa en la persistencia
+ *
+ * @author Phelyxx
+ */
 
 @Getter
 @Setter
 @Entity
 public class EnterpriseEntity extends BaseEntity {
-	// protected PersonEntity representative;
 	
+	@PodamExclude
+	@OneToOne
+	protected PersonEntity representative;
 }
