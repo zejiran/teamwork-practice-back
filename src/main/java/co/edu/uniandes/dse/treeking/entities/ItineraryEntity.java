70b1a354aad1de6fa6d6efecd542d091956fc04f
Santiago Pardo <s.pardob@uniandes.edu.co>
2021-08-31 22:32:47 -0500

    Itinerary Implementado con pruebas

---
+++ src/main/java/co/edu/uniandes/dse/treeking/entities/ItineraryEntity.java
@@ -0,0 +1,16 @@
+package co.edu.uniandes.dse.treeking.entities;
+
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class ItineraryEntity extends BaseEntity{
+	
+	private String lodging;
+	
+	private String transportation;
+}
