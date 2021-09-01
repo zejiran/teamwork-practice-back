543aef6d425bd4293c6ae6dd803608afdc29f616
TrashCam9 <mc.gomezh1@uniandes.edu.co>
2021-08-31 20:28:35 -0500

    Clases sin implementacion para el funcionamiento de entidades ya implementadas

---
+++ src/main/java/co/edu/uniandes/dse/treeking/entities/ComplementaryInformationEntity.java
@@ -0,0 +1,14 @@
+package co.edu.uniandes.dse.treeking.entities;
+
+
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class ComplementaryInformationEntity extends BaseEntity {
+	//TODO: add atributes of ComplementaryInformation
+}
