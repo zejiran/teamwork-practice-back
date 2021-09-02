f4d62de415f8eb0ada3729d2b7ee09d243e2ee94
Net <j.diazi@uniandes.edu.co>
2021-09-01 23:55:39 -0500

    arreglo de indentacion

--- src/main/java/co/edu/uniandes/dse/treeking/entities/BlogEntity.java
+++ src/main/java/co/edu/uniandes/dse/treeking/entities/BlogEntity.java
@@ -4,12 +4,15 @@ import lombok.Setter;
 import uk.co.jemos.podam.common.PodamExclude;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
 
 @Entity
 @Getter
 @Setter
 public class BlogEntity extends BaseEntity{
     @PodamExclude
-	  @OneToMany(mappedBy = "blog", fetch = FetchType.LAZY)  
-    private ArrayList<PostEntity> blogPosts
+    @OneToMany(mappedBy = "blog", fetch = FetchType.LAZY)
+    private ArrayList<PostEntity> blogPosts;
 }
--- src/main/java/co/edu/uniandes/dse/treeking/entities/CommentEntity.java
+++ src/main/java/co/edu/uniandes/dse/treeking/entities/CommentEntity.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import uk.co.jemos.podam.common.PodamExclude;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 
