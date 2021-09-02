faf49f7689632959c28d4f091971c170ad9fa27a
Juan Camach <68751794+BalaclavaAM@users.noreply.github.com>
2021-09-01 20:52:41 -0500

    Ya corre el test correctamente.

--- src/main/java/co/edu/uniandes/dse/treeking/entities/CalendarEntity.java
+++ src/main/java/co/edu/uniandes/dse/treeking/entities/CalendarEntity.java
@@ -1,11 +1,18 @@
 package co.edu.uniandes.dse.treeking.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 
+@Setter
+@Getter
+@Entity
 public class CalendarEntity extends BaseEntity{
-    /**
-     * no existe quotation entity x ahora :(
+    /*
+      no existe quotation entity x ahora :(
     @ManyToOne
     private QuotationEntity history;
-    **/
+    */
 }
