669d57e8addeee483a55cdca045df25ed8358022
TrashCam9 <mc.gomezh1@uniandes.edu.co>
2021-09-06 23:51:39 -0500

    Implementación de clase InsuranceDTO completada

---
+++ src/main/java/co/edu/uniandes/dse/treeking/dto/InsuranceDTO.java
@@ -0,0 +1,11 @@
+package co.edu.uniandes.dse.treeking.dto;
+
+import lombok.Data;
+
+@Data
+public class InsuranceDTO {
+	private String name;
+	private String conditions;
+	private String features;
+	private Integer price;
+}
