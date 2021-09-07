2b67a6a034b0c7d126dac5c2aa73806a307b634d
TrashCam9 <mc.gomezh1@uniandes.edu.co>
2021-09-07 00:19:19 -0500

    Implementación de la clase básica y detalla de Quotation para el DTO

---
+++ src/main/java/co/edu/uniandes/dse/treeking/dto/QuotationDTO.java
@@ -0,0 +1,10 @@
+package co.edu.uniandes.dse.treeking.dto;
+
+import lombok.Data;
+
+@Data
+public class QuotationDTO {
+	private Integer participants;
+	private Float totalCost;
+	private OutingDTO outing;
+}
