8250db60b78c8fe1445396fb7163012fa74ff560
TrashCam9 <mc.gomezh1@uniandes.edu.co>
2021-09-07 00:24:24 -0500

    Atributo status añadido

--- src/main/java/co/edu/uniandes/dse/treeking/dto/QuotationDTO.java
+++ src/main/java/co/edu/uniandes/dse/treeking/dto/QuotationDTO.java
@@ -1,11 +1,13 @@
 package co.edu.uniandes.dse.treeking.dto;
 
 import lombok.Data;
+import co.edu.uniandes.dse.treeking.enums.Status;
 
 @Data
 public class QuotationDTO {
 	private Long id;
 	private Integer participants;
 	private Float totalCost;
+	private Status status;
 	private OutingDTO outing;
 }
