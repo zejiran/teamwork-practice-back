795244613310183e1db39ba7125221fe55b78693
Phelyxx <habbowalkvps@gmail.com>
2021-09-08 10:21:16 -0500

    Add DTO with controller of Transaction
    TO-DO empty classes

---
+++ src/main/java/co/edu/uniandes/dse/treeking/dto/TransactionDTO.java
@@ -0,0 +1,15 @@
+package co.edu.uniandes.dse.treeking.dto;
+
+import java.util.Date;
+
+import co.edu.uniandes.dse.treeking.enums.PaymentType;
+
+import lombok.Data;
+
+@Data
+public class TransactionDTO {
+	private PaymentType payment;
+	private Integer value;
+	private Date creationDate;
+	private ClientDTO client;
+}
