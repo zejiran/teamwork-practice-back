048076d4cf0636802aa859401749c7db67aa307c
Juan Camach <68751794+BalaclavaAM@users.noreply.github.com>
2021-09-08 18:38:58 -0500

    Update src/main/java/co/edu/uniandes/dse/treeking/dto/RouteDetailDTO.java
    Co-authored-by: Juan Alegría <juanszalegria@gmail.com>

--- src/main/java/co/edu/uniandes/dse/treeking/dto/RouteDetailDTO.java
+++ src/main/java/co/edu/uniandes/dse/treeking/dto/RouteDetailDTO.java
@@ -8,7 +8,7 @@ import java.util.List;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class RouteDetailDTO extends RouteDTO {
-    private List<MultimediaDTO> multimediaDTO;
+    private List<MultimediaDTO> multimediasDTO;
     private List<ReviewDTO> reviewDTO;
     private List<LocationDTO> locationsDTOS;
 }
