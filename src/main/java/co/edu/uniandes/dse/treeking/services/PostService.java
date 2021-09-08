8a52410b8cb5a7df2c3fb150b4930858c5f03964
Phelyxx <habbowalkvps@gmail.com>
2021-09-08 11:27:04 -0500

    Add service annotation

--- src/main/java/co/edu/uniandes/dse/treeking/services/PostService.java
+++ src/main/java/co/edu/uniandes/dse/treeking/services/PostService.java
@@ -3,11 +3,13 @@ package co.edu.uniandes.dse.treeking.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import co.edu.uniandes.dse.treeking.entities.PostEntity;
 import co.edu.uniandes.dse.treeking.repositories.PostRepository;
 
+@Service
 public class PostService {
 	@Autowired
 	private PostRepository postRepository;
