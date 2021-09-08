f8c32d13a12f6483845cbb1a732565abeb8134bb
Juan Alegría <juanszalegria@gmail.com>
2021-09-08 11:24:30 -0500

    Feat: add Person detail DTO

---
+++ src/main/java/co/edu/uniandes/dse/treeking/dto/PersonDetailDTO.java
@@ -0,0 +1,10 @@
+package co.edu.uniandes.dse.treeking.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
+public class PersonDetailDTO extends PersonDTO {
+
+}
