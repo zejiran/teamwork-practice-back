723e7656480484fc5f57a258e8d0987e5214ad17
Santiago Pardo <s.pardob@uniandes.edu.co>
2021-09-08 20:17:11 -0500

    Implement activityDto

---
+++ src/main/java/co/edu/uniandes/dse/treeking/controllers/ActivityController.java
@@ -0,0 +1,38 @@
+package co.edu.uniandes.dse.treeking.controllers;
+
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.uniandes.dse.treeking.dto.ActivityDTO;
+import co.edu.uniandes.dse.treeking.entities.ActivityEntity;
+import co.edu.uniandes.dse.treeking.services.ActivityService;
+
+@RestController
+@RequestMapping("/activities")
+public class ActivityController {
+
+	@Autowired
+    private ActivityService activityService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    
+    @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
+    public List<ActivityDTO> findAll() {
+            List<ActivityEntity> activities = activityService.getActivities();
+            return modelMapper.map(activities, new TypeToken<List<ActivityDTO>>() {
+            }.getType());
+    }
+
+
+}
--- src/main/java/co/edu/uniandes/dse/treeking/dto/ActivityDTO.java
+++ src/main/java/co/edu/uniandes/dse/treeking/dto/ActivityDTO.java
@@ -1,8 +1,22 @@
 package co.edu.uniandes.dse.treeking.dto;
 
+import java.time.LocalDateTime;
+
+import co.edu.uniandes.dse.treeking.entities.OutingEntity;
 import lombok.Data;
 
 @Data
 public class ActivityDTO {
 
+	private Long id;
+	
+	private String name;
+
+	private String description;
+
+	private LocalDateTime time;
+
+	private Integer minutes;
+	
+	private OutingDTO outing;
 }
