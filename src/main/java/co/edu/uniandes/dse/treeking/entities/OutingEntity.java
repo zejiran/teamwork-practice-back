230532ae3e3719be6da756edcdc884eb15c6b1de
netandout <j.diazi@uniandes.edu.co>
2021-10-25 03:51:13 -0500

    arreglo en los test

--- src/main/java/co/edu/uniandes/dse/treeking/entities/OutingEntity.java
+++ src/main/java/co/edu/uniandes/dse/treeking/entities/OutingEntity.java
@@ -1,16 +1,14 @@
 package co.edu.uniandes.dse.treeking.entities;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import javax.persistence.*;
-
 import co.edu.uniandes.dse.treeking.podam.DateStrategy;
 import lombok.Getter;
 import lombok.Setter;
 import uk.co.jemos.podam.common.PodamExclude;
 import uk.co.jemos.podam.common.PodamStrategyValue;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
@@ -45,7 +43,6 @@ public class OutingEntity extends BaseEntity {
 	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	private GuideEntity guide; //considero que solo debería tener un guia, si pudiesen haber mas, seria manyToMany
 
-	@PodamExclude
 	@OneToMany(mappedBy = "outing", fetch = FetchType.LAZY)
 	private List<QuotationEntity> quotations;
 
