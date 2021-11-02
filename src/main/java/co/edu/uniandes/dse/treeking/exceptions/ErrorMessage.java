fa24b0fe1bac3295bf4c603c7080243796e04509
TrashCam9 <mc.gomezh1@uniandes.edu.co>
2021-11-02 17:34:10 -0500

    Add new ErrorMessage for INSURANCE_NOT_FOUND error

--- src/main/java/co/edu/uniandes/dse/treeking/exceptions/ErrorMessage.java
+++ src/main/java/co/edu/uniandes/dse/treeking/exceptions/ErrorMessage.java
@@ -2,7 +2,9 @@ package co.edu.uniandes.dse.treeking.exceptions;
 
 public class ErrorMessage {
 	public static final String QUOTATION_NOT_FOUND = "The quotation with the given id was not found";
-
+	
+	public static final String INSURANCE_NOT_FOUND = "The quotation with the given id was not found";
+	
 	private ErrorMessage() {
 		throw new IllegalStateException("Utility class");
 	}
