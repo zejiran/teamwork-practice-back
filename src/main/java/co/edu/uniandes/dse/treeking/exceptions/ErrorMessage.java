2e2095aa441f87e94fd372be2f6fcfb4ec98c7cf
Camila Gómez <62612964+TrashCam9@users.noreply.github.com>
2021-11-02 18:55:41 -0500

    Update ErrorMessage.java

--- src/main/java/co/edu/uniandes/dse/treeking/exceptions/ErrorMessage.java
+++ src/main/java/co/edu/uniandes/dse/treeking/exceptions/ErrorMessage.java
@@ -3,7 +3,7 @@ package co.edu.uniandes.dse.treeking.exceptions;
 public class ErrorMessage {
 	public static final String QUOTATION_NOT_FOUND = "The quotation with the given id was not found";
 
-	public static final String INSURANCE_NOT_FOUND = "The quotation with the given id was not found";
+	public static final String INSURANCE_NOT_FOUND = "The insurance with the given id was not found";
 
 	public static final String OUTING_NOT_FOUND = "The outing with the given id was not found";
 
