98184c50165d2e342ab7da9be5fd78b9959d074b
Santiago Pardo <s.pardob@uniandes.edu.co>
2021-11-03 16:39:18 -0500

    ActivityDetail with test

--- src/main/java/co/edu/uniandes/dse/treeking/exceptions/ErrorMessage.java
+++ src/main/java/co/edu/uniandes/dse/treeking/exceptions/ErrorMessage.java
@@ -1,13 +1,21 @@
 package co.edu.uniandes.dse.treeking.exceptions;
 
 public class ErrorMessage {
-	public static final String QUOTATION_NOT_FOUND = "The quotation with the given id was not found";
+	public static final String QUOTATION_NOT_FOUND = "The quotation with the given ID was not found";
 
-	public static final String INSURANCE_NOT_FOUND = "The insurance with the given id was not found";
+	public static final String INSURANCE_NOT_FOUND = "The insurance with the given ID was not found";
 
-	public static final String OUTING_NOT_FOUND = "The outing with the given id was not found";
+	public static final String OUTING_NOT_FOUND = "The outing with the given ID was not found";
 
-	public static final String CLIENT_NOT_FOUND = "The client with the given id was not found";
+	public static final String CLIENT_NOT_FOUND = "The client with the given ID was not found";
+	
+	public static final String ACTIVITY_NOT_FOUND = "The activity with the given ID was not found";
+
+	public static final String COMPLEMENTARYINFORMATION_NOT_FOUND = "The complementary information with the given ID was not found";
+	
+	public static final String GUIDE_NOT_FOUND = "The guide with the given ID was not found";
+	
+	public static final String ITINERARY_NOT_FOUND = "The itinerary with the given ID was not found";
 
 	private ErrorMessage() {
 		throw new IllegalStateException("Utility class");
