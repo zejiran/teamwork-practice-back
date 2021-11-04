513cc1d0b63374f9bb713ef7efa3f76a13a952b7
Juan Camach <68751794+BalaclavaAM@users.noreply.github.com>
2021-11-03 21:09:02 -0500

    getById methods added

--- src/main/java/co/edu/uniandes/dse/treeking/services/CalendarService.java
+++ src/main/java/co/edu/uniandes/dse/treeking/services/CalendarService.java
@@ -1,7 +1,11 @@
 package co.edu.uniandes.dse.treeking.services;
 
 import java.util.List;
+import java.util.Optional;
 
+import co.edu.uniandes.dse.treeking.entities.RouteEntity;
+import co.edu.uniandes.dse.treeking.exceptions.EntityNotFoundException;
+import co.edu.uniandes.dse.treeking.exceptions.ErrorMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,5 +25,13 @@ public class CalendarService {
 	@Transactional
 	public List<CalendarEntity> getCalendars() {
 		return calendarRepository.findAll();
+	}
+
+	public CalendarEntity getCalendarById(Integer id) throws EntityNotFoundException {
+		Optional<CalendarEntity> calendarEntity = Optional.of(calendarRepository.getById(Long.valueOf(id)));
+		if (calendarEntity.isEmpty()) {
+			throw new EntityNotFoundException(ErrorMessage.ROUTE_NOT_FOUND);
+		}
+		return calendarEntity.get();
 	}
 }
--- src/main/java/co/edu/uniandes/dse/treeking/services/RouteService.java
+++ src/main/java/co/edu/uniandes/dse/treeking/services/RouteService.java
@@ -1,13 +1,18 @@
 package co.edu.uniandes.dse.treeking.services;
 
 import java.util.List;
+import java.util.Optional;
 
+import co.edu.uniandes.dse.treeking.entities.LocationEntity;
+import co.edu.uniandes.dse.treeking.exceptions.EntityNotFoundException;
+import co.edu.uniandes.dse.treeking.exceptions.ErrorMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import co.edu.uniandes.dse.treeking.entities.RouteEntity;
 import co.edu.uniandes.dse.treeking.repositories.RouteRepository;
+import org.springframework.util.RouteMatcher;
 
 @Service
 public class RouteService {
@@ -27,5 +32,12 @@ public class RouteService {
 		} else {
 			return null;
 		}
+	}
+	public RouteEntity getRouteById(Integer id) throws EntityNotFoundException {
+		Optional<RouteEntity> routeEntity = Optional.of(routeRepository.getById(Long.valueOf(id)));
+		if (routeEntity.isEmpty()) {
+			throw new EntityNotFoundException(ErrorMessage.ROUTE_NOT_FOUND);
+		}
+		return routeEntity.get();
 	}
 }
