685584ac952ae971f2424d180e1313816d42dee5
netandout <62193128+netandout@users.noreply.github.com>
2021-11-08 00:39:22 -0500

    Update src/main/java/co/edu/uniandes/dse/treeking/dto/MultimediaDTO.java
    Co-authored-by: Juan Alegría <juanszalegria@gmail.com>

--- src/main/java/co/edu/uniandes/dse/treeking/dto/MultimediaDTO.java
+++ src/main/java/co/edu/uniandes/dse/treeking/dto/MultimediaDTO.java
@@ -8,6 +8,5 @@ public class MultimediaDTO {
 	private String fileName;
 	private FileType mediaType;
 	private String extension;
-	// esto no deberia estar en el DTO es solo algo para la persistencia
 	// esto no deberia estar en el DTO
 }
