b9923c35d402669431714266fe17620abe248c49
netandout <netiel136@outlook.com>
2021-11-08 02:57:23 -0500

    controller : Admin, Blog, Comment, Review

--- src/main/java/co/edu/uniandes/dse/treeking/controllers/BlogController.java
+++ src/main/java/co/edu/uniandes/dse/treeking/controllers/BlogController.java
@@ -1,15 +1,15 @@
 package co.edu.uniandes.dse.treeking.controllers;
+import co.edu.uniandes.dse.treeking.dto.BlogDetailDTO;
 import co.edu.uniandes.dse.treeking.dto.BlogDTO;
 import co.edu.uniandes.dse.treeking.entities.BlogEntity;
+import co.edu.uniandes.dse.treeking.entities.BlogEntity;
+import co.edu.uniandes.dse.treeking.exceptions.EntityNotFoundException;
 import co.edu.uniandes.dse.treeking.services.BlogService;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +30,12 @@ public class BlogController {
         List<BlogEntity> blogs = blogService.getBlogs();
         return modelMapper.map(blogs, new TypeToken<List<BlogDTO>>() {
         }.getType());
+    }
+
+    @GetMapping(value = "/{id}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public BlogDetailDTO findOne(@PathVariable("id") Long id) throws EntityNotFoundException {
+        BlogEntity entity = blogService.getBlog(id);
+        return modelMapper.map(entity, BlogDetailDTO.class);
     }
 }
