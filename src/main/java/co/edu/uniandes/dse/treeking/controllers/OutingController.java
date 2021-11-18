86759fd7892160caaf07fe93696f423abef4eec7
TrashCam9 <mc.gomezh1@uniandes.edu.co>
2021-11-18 17:59:57 -0500

    Test for createOuting implemented in outingServiceTest

--- src/main/java/co/edu/uniandes/dse/treeking/controllers/OutingController.java
+++ src/main/java/co/edu/uniandes/dse/treeking/controllers/OutingController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,7 @@ import co.edu.uniandes.dse.treeking.dto.OutingDTO;
 import co.edu.uniandes.dse.treeking.dto.OutingDetailDTO;
 import co.edu.uniandes.dse.treeking.entities.OutingEntity;
 import co.edu.uniandes.dse.treeking.exceptions.EntityNotFoundException;
+import co.edu.uniandes.dse.treeking.exceptions.IllegalOperationException;
 import co.edu.uniandes.dse.treeking.services.OutingService;
 
 @RestController
@@ -55,5 +58,21 @@ public class OutingController {
 	public OutingDetailDTO findOne(@PathVariable("id") Long id) throws EntityNotFoundException {
 		OutingEntity outingEntity = outingService.getOuting(id);
 		return modelMapper.map(outingEntity, OutingDetailDTO.class);
+	}
+	
+	/**
+	 * Crea un nuevo outing con la informacion que se recibe en el cuerpo de la
+	 * petición y se regresa un objeto identico con un id auto-generado por la base
+	 * de datos.
+	 *
+	 * @param book {@link OutingDTO} - EL outing que se desea guardar.
+	 * @return JSON {@link OutingDTO} - El outing guardado con el atributo id
+	 *         autogenerado.
+	 */
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public OutingDTO create(@RequestBody OutingDTO outingDTO) throws IllegalOperationException, EntityNotFoundException {
+		OutingEntity outingEntity = outingService.createOuting(modelMapper.map(outingDTO, OutingEntity.class));
+		return modelMapper.map(outingEntity, OutingDTO.class);
 	}
 }
