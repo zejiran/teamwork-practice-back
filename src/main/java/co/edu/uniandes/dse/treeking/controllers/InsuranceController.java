a556abb097a4f51053ac68caaf3a956aa366440c
TrashCam9 <mc.gomezh1@uniandes.edu.co>
2021-11-24 15:14:22 -0500

    create method added to InsuranceController

--- src/main/java/co/edu/uniandes/dse/treeking/controllers/InsuranceController.java
+++ src/main/java/co/edu/uniandes/dse/treeking/controllers/InsuranceController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,7 @@ import co.edu.uniandes.dse.treeking.dto.InsuranceDTO;
 import co.edu.uniandes.dse.treeking.dto.InsuranceDetailDTO;
 import co.edu.uniandes.dse.treeking.entities.InsuranceEntity;
 import co.edu.uniandes.dse.treeking.exceptions.EntityNotFoundException;
+import co.edu.uniandes.dse.treeking.exceptions.IllegalOperationException;
 import co.edu.uniandes.dse.treeking.services.InsuranceService;
 
 @RestController
@@ -54,6 +57,22 @@ public class InsuranceController {
 	public InsuranceDetailDTO findOne(@PathVariable("id") Long id) throws EntityNotFoundException {
 		InsuranceEntity insuranceEntity = insuranceService.getInsurance(id);
 		return modelMapper.map(insuranceEntity, InsuranceDetailDTO.class);
+	}
+	
+	/**
+	 * Crea un nuevo insurance con la informacion que se recibe en el cuerpo de la
+	 * petición y se regresa un objeto identico con un id auto-generado por la base
+	 * de datos.
+	 *
+	 * @param book {@link InsuranceDTO} - EL insurance que se desea guardar.
+	 * @return JSON {@link InsuranceDTO} - El insurance guardado con el atributo id
+	 *         autogenerado.
+	 */
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public InsuranceDTO create(@RequestBody InsuranceDTO insuranceDTO) throws IllegalOperationException, EntityNotFoundException {
+		InsuranceEntity insuranceEntity = insuranceService.createInsurance(modelMapper.map(insuranceDTO, InsuranceEntity.class));
+		return modelMapper.map(insuranceEntity, InsuranceDTO.class);
 	}
 
 }
