a7a5e2fc182503e72cd75fd3c5bb5fc5447928e1
Juan Alegría <j.alegria@uniandes.edu.co>
2021-12-03 01:16:26 -0500

    Fix: update multimedia detail attributes

--- src/main/java/co/edu/uniandes/dse/treeking/dto/MultimediaDTO.java
+++ src/main/java/co/edu/uniandes/dse/treeking/dto/MultimediaDTO.java
@@ -1,9 +1,12 @@
 package co.edu.uniandes.dse.treeking.dto;
 
+import co.edu.uniandes.dse.treeking.enums.FileType;
 import lombok.Data;
 
 @Data
 public class MultimediaDTO {
 	private Long id;
 	private String fileName;
+	private FileType mediaType;
+	private String extension;
 }
